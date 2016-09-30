9c22b50b3d8841b40d9f3b85e1e22e6ff18b5388
budzanowski <bartosz.budzanowski@gmail.com>
2016-09-30 12:53:54 +0200

    Add mouse events.

--- client/my-sites/themes/themes-magic-search-card/index.jsx
+++ client/my-sites/themes/themes-magic-search-card/index.jsx
@@ -42,6 +42,7 @@ const ThemesMagicSearchCard = React.createClass( {
 	},
 
 	componentDidMount() {
+		this.findTextForSuggestions( this.props.search );
 		window.addEventListener( 'resize', this.onResize );
 	},
 
@@ -73,8 +74,11 @@ const ThemesMagicSearchCard = React.createClass( {
 	},
 
 	onKeyDown( event ) {
-		const text = this.findTextForSuggestions( event.target.value );
-		this.setState( { editedSearchElement: text } );
+		this.findTextForSuggestions( event.target.value );
+	},
+
+	onClick( event ) {
+		this.findTextForSuggestions( event.target.value );
 	},
 
 	findEditedToken( tokens, cursorPosition ) {
@@ -96,7 +100,7 @@ const ThemesMagicSearchCard = React.createClass( {
 				//if this one is white space only next
 				//next one must be text
 				const moreTokensExist = i < tokens.length - 1;
-				if(  tokenIsWhiteSpace && moreTokensExist ) {
+				if( tokenIsWhiteSpace && moreTokensExist ) {
 					return tokens[ i + 1 ];
 				} else {
 					// "" indicates full suggestion request
@@ -114,22 +118,21 @@ const ThemesMagicSearchCard = React.createClass( {
 	findTextForSuggestions( input ) {
 		let val = input;
 		const _this = this;
-		//Do we realy need to do this? pobably to delete
 		window.requestAnimationFrame(function() {
-			_this.setState( { cursorPosition: val.slice(0, _this.refs['url-search'].refs.searchInput.selectionStart).length } );
+			_this.setState( { cursorPosition: val.slice( 0, _this.refs['url-search'].refs.searchInput.selectionStart ).length } );
+			console.log( _this.state.cursorPosition ) ;
+			const tokens = input.split(/(\s+)/);
+
+			// Get rid of empty match at end
+			tokens[tokens.length -1] === "" && tokens.splice( tokens.length - 1 , 1 );
+
+			const text =  _this.findEditedToken( tokens, _this.state.cursorPosition );
+			_this.setState( { editedSearchElement: text } );
 		} );
-
-		console.log(this.state.cursorPosition);
-		const tokens = input.split(/(\s+)/);
-
-		// Get rid of empty match at end
-		tokens[tokens.length -1] === "" && tokens.splice(tokens.length - 1 , 1 );
-		return this.findEditedToken( tokens, this.state.cursorPosition );
 	},
 
 	onSearchChange( input ) {
-		const text = this.findTextForSuggestions( input );
-		this.setState( { editedSearchElement: text } );
+		this.findTextForSuggestions( input );
 		this.setState( {searchInput: input } );
 	},
 
@@ -177,6 +180,7 @@ const ThemesMagicSearchCard = React.createClass( {
 				onSearchClose={ this.onSearchClose }
 				onSearchChange={ this.onSearchChange }
 				onKeyDown={ this.onKeyDown }
+				onClick={ this.onClick }
 				overlayStyling={ this.searchTokens }
 				onBlur={ this.onBlur }
 				fitsContainer={ this.state.isMobile && this.state.searchIsOpen }
